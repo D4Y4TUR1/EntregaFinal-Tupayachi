3157665d38f1973d3ad7d3e9b3988e6d96f33f9a
D4Y4TUR1 <david.tupayachirivas@gmail.com>
2024-02-13 00:02:12 -0500

    2da Pre-entrega v3

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,7 @@
 import './App.css'
 import NavBar from './components/NavBar/NavBar'
-import Contenedor from './components/Contenedor/Contenedor'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 function App() {
-
-  const productos = [
-    {
-      "producto": "Guantes",
-      "precio": "20",
-      "stock": "10"
-    },
-
-    {
-      "producto": "Straps",
-      "precio": "25",
-      "stock": "12"
-    },
-
-    {
-      "producto": "Cinturones",
-      "precio": "40",
-      "stock": "5"
-    },
-
-    {
-      "producto": "Toalla",
-      "precio": "6",
-      "stock": "20"  
-    }
-  ]
 
   return (
     <>
--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import Item from '../Item/Item'
-
 const ItemList = ({productos}) => {
     return (
         <>
-            {
+        
+        {
                 productos.map((producto) =>{
                     return (
+                        
                         <Item producto={producto}/>
+                        
                     )
                 })
             }
+        
+            
         </>
     )
 }
